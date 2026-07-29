48d9cddc50bc7b97838b7c94f2614a86253b3409
MuhammadZaqi732 <muhammadzaq732@gmail.com>
2026-07-29 18:14:19 +0700

    Perbaiki path responsive terbaru

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.container}>
         <p>© 2026 BADAN KEPEGAWAIAN NEGARA</p>
-      </div>
     </footer>
   );
 }
